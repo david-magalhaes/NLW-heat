e9d7803e9c0d2a94ca7cf22d172571f5818f059b
David Magalhães <david.magalhaes@loggi.com>
2021-10-19 18:00:32 -0300

    Implementação do Backend

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,39 @@
 import "dotenv/config";
+import http from "http";
+import cors from "cors";
 import express from "express";
+import { Server } from "socket.io";
 
 import { router } from "./routes";
 
 const app = express();
-app.use(express.json());
+app.use(cors());
 
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
+app.use(express.json());
 app.use(router);
 
-app.get("/github", (request, response) => {
-    response.redirect(
-        `http://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
-    );
+app.get("/github", (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
 });
 
-app.get("/signin/callback", (request, response) => {
-    const { code } = request.query;
+app.get("/signin/callback", (req, res) => {
+  const { code } = req.query;
 
-    return response.json(code);
+  return res.json(code);
 });
 
-
-app.listen(4000, () => console.log('Server is running on port 4000'))
+export { serverHttp, io };
--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -1,16 +1,21 @@
 import { Request, Response } from "express";
-import {AuthenticateUserService} from "../services/AuthenticateUserService";
+
+import { AuthenticateUserService } from "../services/AuthenticateUserService";
 
 class AuthenticateUserController {
-    async handle(request: Request, response: Response){
+  async handle(req: Request, res: Response) {
+    const { code } = req.body;
 
-        const{ code }= request.body
+    const service = new AuthenticateUserService();
 
-        const service = new AuthenticateUserService();
-        const result = await service.execute(code);
+    try {
+      const result = await service.execute(code);
 
-        return response.json(result);
+      return res.json(result);
+    } catch (error) {
+      return res.json({ error: error.message });
     }
+  }
 }
 
-export{ AuthenticateUserController };
+export { AuthenticateUserController };
