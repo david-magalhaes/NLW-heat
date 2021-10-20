759d9f2ba9ae4cd6fd5776b8d410de1d5052b46a
David Magalhães <david.magalhaes@loggi.com>
2021-10-19 21:52:44 -0300

    Inicio da implementação do front-end

---
+++ web/src/components/LoginBox/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss';
+
+export function LoginBox(){
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe a sua mensagem</strong>
+      <a href="#" className={styles.signInWithGithub}>
+        Entrar com Github
+      </a>
+    </div>
+  )
+}
